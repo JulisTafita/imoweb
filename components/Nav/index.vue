c2a8cc1faa23fedb0698a677b6c353bc7b1ebc15
Julis <julis@thegreenshot.green>
2023-10-16 18:41:31 +0200

    modify change

--- components/Nav/index.vue
+++ components/Nav/index.vue
@@ -1,19 +1,42 @@
 <template>
-    <nav class="flex justify-center br-[gray] ">
-        <div class="lg:mx-5 my-2 ">
-            <img class="h-20 w-40 " src="/img/nativel_icon.jpg" alt="Discover Nuxt 3" />
-        </div>
-        <div class="flex justify-center mt-4">
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3">home</div>
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 rounded-md border-2 border-[#00738140]">Estimer votre bien</div>
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Acheter</div>
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Louer</div>
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Trouver voter agence</div>
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Conseil</div>
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Medias</div>
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Rejoignez-nous</div>
-             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">connexion</div> 
-        </div>
-    </nav>
+    <div class="container mx-auto">
+        <nav class="flex flex-row justify-center items-center">
+            <div class="basis-1/4 flex flex-row justify-end">
+                <NuxtLink to="/"
+                          prefetchedClass="text-gray-800 text-xl font-bold md:text-2xl hover:text-gray-700">
+                    <img class="h-20 w-40" src="/img/nativel_icon.jpg" alt="Discover Nuxt 3"/>
+                </NuxtLink>
+            </div>
+            <div class="basis-3/4 ">
+                <div class="flex flex-row justify-end">
+                    <NuxtLink to="/"
+                              prefetchedClass="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide"
+                              activeClass="text-cyan-500">
+                        <span>Acceuil</span>
+                    </NuxtLink>
+                    <NuxtLink to="/products"
+                              prefetchedClass="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide"
+                              activeClass="text-cyan-500">
+                        <span>Acheter</span>
+                    </NuxtLink>
+                    <NuxtLink to="/products"
+                              prefetchedClass="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide"
+                              activeClass="text-cyan-500">
+                        <span>Louer</span>
+                    </NuxtLink>
+                    <NuxtLink to="/products"
+                              prefetchedClass="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide"
+                              activeClass="text-cyan-500">
+                        <span>Nous contacter</span>
+                    </NuxtLink>
+                    <NuxtLink to="/admin"
+                              prefetchedClass="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide"
+                              activeClass="text-cyan-500">
+                        <span>ADMIN</span>
+                    </NuxtLink>
+                </div>
+            </div>
+        </nav>
+    </div>
 </template>
 

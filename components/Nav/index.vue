d16c6af4d30c48ea4454b4bcb6d39847f1002ac5
AndriantsoaFaniry <139750601+AndriantsoaFaniry@users.noreply.github.com>
2023-10-16 19:18:18 +0300

    navigation bar

--- components/Nav/index.vue
+++ components/Nav/index.vue
@@ -1,3 +1,19 @@
 <template>
-    <div>This is a sample navbar</div>
+    <nav class="flex justify-center br-[gray] ">
+        <div class="lg:mx-5 my-2 ">
+            <img class="h-20 w-40 " src="/img/nativel_icon.jpg" alt="Discover Nuxt 3" />
+        </div>
+        <div class="flex justify-center mt-4">
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3">home</div>
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 rounded-md border-2 border-[#00738140]">Estimer votre bien</div>
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Acheter</div>
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Louer</div>
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Trouver voter agence</div>
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Conseil</div>
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Medias</div>
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">Rejoignez-nous</div>
+             <div class="lg:mx-1 my-2 lg:block hidden font-bold text-sm h-12 pt-3 px-3 tracking-wide">connexion</div> 
+        </div>
+    </nav>
 </template>
+

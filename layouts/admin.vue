ce3dafd14501474b2caf2c2de6f4e41530ee3a3e
Julis <julis@thegreenshot.green>
2023-10-16 18:12:48 +0200

    admin page

---
+++ layouts/admin.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="min-h-screen flex flex-row flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
+        <div class="flex flex-col basis-1/6 bg-white h-screen border-r">
+            <AdminSideBar/>
+        </div>
+
+        <div class="bases-3/4">
+            <slot></slot>
+        </div>
+    </div>
+</template>
---
+++ pages/admin/dashboard.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+definePageMeta({
+    layout: "admin",
+});
+
+
+</script>
+
+
+<template>
+    <div>THIS IS A PAGE......................................................................................;</div>
+</template>
